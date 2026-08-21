b2ece9536469cd858b664634290b625a2e283a96
Evans Njenga <tindievans0110@gmail.com>
2026-08-21 11:34:01 +0300

    logging page

--- js file.js
+++
@@ -1,8 +0,0 @@
-let text1 ="hello";
-let text2 ="world!";
-let greet = `wakenya, ${text1}  ${text2} kwenu nyote `;
-console.log(greet);
-
-let text = "Apple, Banana, Kiwi";
-let part = text.slice(7);
-console.log(part);
