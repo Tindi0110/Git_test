8375e983d711f8156e7d1a7b407314161d043a4f
Evans Njenga <tindievans0110@gmail.com>
2026-08-23 10:39:14 +0300

    javascript modification

--- javascript.js
+++ javascript.js
@@ -1,20 +1,8 @@
-const pwdInput = document.querySelector("#password");
-const eyeIcon= document.querySelector("#eye-icon");
-
-eyeIcon.addEventListener("click", toggleInput )
-function toggleInput(){
-    let currenttype = pwdInput.getAttribute("type");
-    if (currenttype === 'password') {
-        
-        eyeIcon.classList.remove('fa-eye-slash');
-        eyeIcon.classList.add('fa-eye');
-        pwdInput.setAttribute('type', 'text');
-
-
+function min(a,b){
+    if (a<b){
+        return a;
+    }else{
+        return b;
     }
-    else {
-        eyeIcon.classList.remove('fa-eye');
-        eyeIcon.classList.add('fa-eye-slash');
-        pwdInput.setAttribute('type', 'password')
-    }
+
 }
